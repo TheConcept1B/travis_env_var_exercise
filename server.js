432c877e256b546a77cb2ffecd63e4298dede287
duytn2 <duytn2@fpt.com>
2023-08-08 21:59:31 +0700

    add use variable

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
+const favoriteFood = process.env.FAVORITE_FOOD;
 
 async function main() {
-  while(true) {
-    console.log("Microservices rock!");
-    await sleep(5000);
-  }
+	while (true) {
+		console.log("Microservices rock!");
+		console.log(`My favorite food is ${favoriteFood}`);
+		await sleep(5000);
+	}
 }
 
 main();
